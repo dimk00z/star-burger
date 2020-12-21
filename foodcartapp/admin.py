9523493cf8fee4ba0f4a191b0bf5e69975ecb9a4
Dmitry <dimk00z@gmail.com>
2020-12-21 13:32:40 +0300

    register order to admin panel

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -6,6 +6,8 @@ from .models import Product
 from .models import ProductCategory
 from .models import Restaurant
 from .models import RestaurantMenuItem
+from .models import Order
+from .models import OderItem
 
 
 class RestaurantMenuItemInline(admin.TabularInline):
@@ -103,3 +105,14 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(ProductCategory)
 class ProductAdmin(admin.ModelAdmin):
     pass
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OderItem
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderItemInline,
+    ]
